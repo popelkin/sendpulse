78d7258d29966ccb358959e3b86f1b386ef02447
popelkin <shurik.wps@gmail.com>
2018-12-12 20:12:40 +0200

    cache ignore fix

--- www/cache/4d3de10a684305dcb1ad0549d144e4976e020ad1.php
+++
@@ -1,4 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <h1>index</h1>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
